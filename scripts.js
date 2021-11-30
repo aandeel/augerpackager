cb2e6117f56dd155a35d62e7fc9f7bb9115bf5ad
Chopper2112 <30455406+Chopper2112@users.noreply.github.com>
2021-11-30 17:31:50 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@
 
 
 const maxScreenshotCount = 5
-const templatesLoc = 'templates/'
+const templatesLoc = 'https://raw.githubusercontent.com/Chopper2112/augerpackager/gh-pages/templates/'
 const validFileColor = 'skyblue'
 const invalidFileColor = 'rgb(255, 133, 133)'
 
